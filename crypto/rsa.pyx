67179da62e09495504f4cd4afab88e37614a2907
Justin Venus <justin.venus@gmail.com>
2013-01-07 22:36:24 -0600

    closer to correct implementation.

--- crypto/rsa.pyx
+++ crypto/rsa.pyx
@@ -80,45 +80,64 @@ ctypedef int (*RSACallback)(
 # Implementation Follows
 ###############################################################################
 
-
-#the real magic happens here
-cdef object _process(object source, RSACallback func, RSA* key):
+cdef object _process(object source, RSACallback func, RSA* key, bint enc):
     """process the source using the provided callback method and rsa key"""
     if not isinstance(source, str):
         raise TypeError("only string data is supported")
     dest = str()
     cdef int read_len
+    cdef int actual = 0
     cdef int i
 
     cdef int dest_buf_size = RSA_size(key)
+    cdef int reserved = dest_buf_size - 11
     cdef unsigned char *dest_buf 
     dest_buf = <unsigned char*>malloc( dest_buf_size )
 
+    # make sure this doesn't find it's way into the try/block below
     if dest_buf is NULL:
         raise MemoryError()
 
     try:
         while source:
             read_len = min(len(source), dest_buf_size)
+        # flen must be less than RSA_size(rsa) - 11 for the PKCS #1 v1.5 based
+        # padding mode.  This caused the encrypt failures on long messages.
+        #
+        # see http://www.openssl.org/docs/crypto/RSA_public_encrypt.html
+        # see http://www.openssl.org/docs/crypto/RSA_private_encrypt.html
+            if enc and bool(read_len > reserved):
+                read_len = reserved
+            #blindly call our C callback function
             i = func(
-                read_len, source, dest_buf,
-                key, RSA_PKCS1_PADDING
+                read_len, <unsigned char*><char*>source,
+                dest_buf, key, RSA_PKCS1_PADDING
             )
+
             if i == -1 or ERR_peek_error():
                 ERR_clear_error()
                 raise ValueError("Operation failed due to invalid input")
-            tmp = <bytes>dest_buf
-            dest += tmp[:i]
+        # Cython doesn't make the unsigned char* to object syntax very clear
+            tmp = <unsigned char*><char*>dest_buf
+            read_len = read_len < i and i or read_len
+            actual += read_len
+            dest += tmp[:read_len]
             source = source[read_len:]
 
-        return dest
+        return dest[0:actual]
     finally:
         free( dest_buf )
 
+###############################################################################
+# Python class definitions follow
+###############################################################################
+import os
 
 cdef class PublicKey:
     """Encrypt text or decrypt data using RSA public key"""
     cdef RSA *_rsa
+    cdef public object id
+    cdef public object path
 
     def __cinit__(self):
         self._rsa = NULL
@@ -138,6 +157,9 @@ cdef class PublicKey:
         if fp is NULL:
             raise MemoryError()
 
+        self.path = path
+        self.id = os.path.basename(path)
+
         cdef EVP_PKEY *evpk
         evpk = PEM_read_PUBKEY(fp, NULL, NULL, NULL)
         x.close() #make sure we close the file
@@ -146,37 +168,42 @@ cdef class PublicKey:
             ERR_clear_error()
             if evpk is not NULL:
                 EVP_PKEY_free(evpk)
-            raise Exception("Failed to read RSA public key %s" % path)
+            raise Exception("Failed to read RSA public key %s" % (path,))
 
         self._rsa = EVP_PKEY_get1_RSA(evpk)
         if ERR_peek_error() != 0 or self._rsa is NULL:
             ERR_clear_error()
             EVP_PKEY_free(evpk)
-            raise Exception("Failed to extract RSA key from the EVP_PKEY at %s" % path) 
+            raise Exception(
+                "Failed to extract RSA key from the EVP_PKEY at %s" % (path,))
 
         #increment the internal rsa struct reference
         RSA_up_ref(self._rsa)
         #release the evp structure
         EVP_PKEY_free(evpk)
 
-    def encrypt(self, source):
-        """encrypt(source) -> string
+    def encrypt(self, text):
+        """encrypt(text) -> data
 
            encrypt text with the rsa public key.
         """
-        return _process(source, <RSACallback>RSA_public_encrypt, self._rsa)
+        return _process(
+            text, <RSACallback>RSA_public_encrypt, self._rsa, True)
 
-    def decrypt(self, source):
-        """decrypt(source) -> string
+    def decrypt(self, data):
+        """decrypt(data) -> text
 
            decrypt data with the rsa public key.
         """
-        return _process(source, <RSACallback>RSA_public_decrypt, self._rsa)
+        return _process(
+            data, <RSACallback>RSA_public_decrypt, self._rsa, False)
 
 
 cdef class PrivateKey:
     """Encrypt text or decrypt data using RSA private key"""
     cdef RSA *_rsa
+    cdef public object id
+    cdef public object path
 
     def __cinit__(self):
         self._rsa = NULL
@@ -202,24 +229,28 @@ cdef class PrivateKey:
         #check for library errors before null pointers
         if ERR_peek_error() != 0:
             ERR_clear_error()
-            raise Exception("Failed to read RSA private key %s" % path)
+            raise Exception("Failed to read RSA private key %s" % (path,))
 
         if self._rsa is NULL:
             raise MemoryError()
 
-    def encrypt(self, source):
-        """encrypt(source) -> string
+        self.path = path
+        self.id = os.path.basename(path).split('.',1)[0]
+
+    def encrypt(self, text):
+        """encrypt(text) -> data
 
            encrypt text with the rsa private key.
         """
-        return _process(source, <RSACallback>RSA_private_encrypt, self._rsa)
+        return _process(
+            text, <RSACallback>RSA_private_encrypt, self._rsa, True)
 
-    def decrypt(self, source):
-        """decrypt(source) -> string
+    def decrypt(self, data):
+        """decrypt(data) -> text
 
            decrypt data with the rsa private key.
         """
-        return _process(source, <RSACallback>RSA_private_decrypt, self._rsa)
-
+        return _process(
+            data, <RSACallback>RSA_private_decrypt, self._rsa, False)
 
 __all__ = ['PrivateKey', 'PublicKey']
